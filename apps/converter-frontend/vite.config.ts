99a0dd6dd19b62034a122d6fadef03ecfc03bc32
Frantisek Farkas <frantisekfarkas93@gmail.com>
2023-11-15 12:32:39 +0100

    fix: vite config

--- apps/converter-frontend/vite.config.ts
+++ apps/converter-frontend/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 
 export default defineConfig({
-  cacheDir: '../../node_modules/.vite/converter-fe',
+  cacheDir: '../../node_modules/.vite/converter-frontend',
 
   server: {
     port: 4200,
