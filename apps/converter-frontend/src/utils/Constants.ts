d023f8e447e84921e31584ccf23e8afbf98cb572
Frantisek Farkas <frantisekfarkas93@gmail.com>
2023-11-16 17:10:07 +0100

    chore: add netlify.toml to api

--- apps/converter-frontend/src/utils/Constants.ts
+++ apps/converter-frontend/src/utils/Constants.ts
@@ -1,1 +1,2 @@
-export const CONVERSION_API_BASE_URL = 'http://localhost:3333/api';
+// export const CONVERSION_API_BASE_URL = 'http://localhost:3333/api';
+export const CONVERSION_API_BASE_URL = 'https://tangerine-cucurucho-5f3e9a.netlify.app/api';
