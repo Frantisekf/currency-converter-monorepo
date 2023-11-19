b1d283212d269f09f4a9f715deb77ed81bff1b81
Frantisek Farkas <frantisekfarkas93@gmail.com>
2023-11-19 21:05:46 +0100

    chore(WIP): hardcode api url

--- apps/converter-frontend/src/utils/Constants.ts
+++ apps/converter-frontend/src/utils/Constants.ts
@@ -1,1 +1,1 @@
-export const CONVERSION_API_BASE_URL = process.env.REACT_APP_CONVERSION_API_BASE_URL || 'http://localhost:3333/api';
+export const CONVERSION_API_BASE_URL = 'https://stark-journey-23101-9dbf0f07c5f0.herokuapp.com/api';
